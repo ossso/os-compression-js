a0a3b9796536292495d3ee15b9776f063d12699f
橙色阳光 <1122@hizhou.cn>
2017-03-14 18:26:45 +0800

    update comment

--- example/auto_3.js
+++ example/auto_3.js
@@ -2,7 +2,7 @@ let auto = require('../index')({
     src: './example/src/', // 源文件路径 支持NodeJS路径规则
     build: './example/dist/', // 目标路径
     buildPre: 'auto_3_', // 随机的前缀
-    random: true, // 关闭随机文件夹
+    random: true, // 是否随机文件夹
     name: 'Auto3',
     exclude: [ // 排除某个JS
         'demo.js'
